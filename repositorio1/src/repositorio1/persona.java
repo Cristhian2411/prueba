a559a2c5deb7d894e930fd6df7ed1a277829181d
cristhian c <cristhianc.lopezg@uqvirtual.edu.co>
2023-03-30 21:05:31 -0500

    2 commit : se creo el constructor y se inicializaron los atributos

--- repositorio1/src/repositorio1/persona.java
+++ repositorio1/src/repositorio1/persona.java
@@ -1,5 +1,12 @@
 package repositorio1;
 
 public class persona {
+	private String nombre ;
+	private String correo;
+	
+	public persona(String nombre, String correo) {
+		this.nombre=nombre;
+		this.correo=correo;
+	}
 
 }
